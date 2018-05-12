5ac7368b99c218d1fe05e11eb31a76a1dd718075
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2018-05-12 19:15:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,3 +37,9 @@ bot.on('guildMemberAdd', member => {
     return channel.send('Bienvenue sur mon serveur ' + member.displayName)
   }).catch(console.error)
 })
+
+bot.on('guildMemberRemove', member => {
+  member.createDM().then(channel => {
+    return channel.send('Pourquoi, es-tu parti :sob: ' + member.displayName)
+  }).catch(console.error)
+})
