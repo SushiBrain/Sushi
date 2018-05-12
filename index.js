050c72ab0f5475b36ace7c13fd9add587fb002e7
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2018-05-12 17:33:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,21 +37,3 @@ bot.on('guildMemberAdd', member => {
     return channel.send('Bienvenue sur mon serveur ' + member.displayName)
   }).catch(console.error)
 })
-
-if(command === "+ban") {
-  if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
-    return message.reply("Tu n'as pas la permission!");
-  
-  let member = message.mentions.members.first();
-  if(!member)
-    return message.reply("Veuillez mentionner un membre valide de ce serveur");
-  if(!member.bannable) 
-    return message.reply("Je ne peux pas interdire cet utilisateur! Ont-ils un rôle plus important? Ai-je des autorisations d'interdiction?");
-
-  let reason = args.slice(1).join(' ');
-  if(!reason) reason = "Aucune raison";
-  
-  await member.ban(reason)
-    .catch(error => message.reply(`Pardon ${message.author} Je ne pouvais pas interdire à cause de : ${error}`));
-  message.reply(`${member.user.tag} a été banni par ${message.author.tag} parceque: ${reason}`);
-}
