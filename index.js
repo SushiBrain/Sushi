0ef174de3572cb2dc1de5e33bacef8d11f350568
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2018-07-21 18:25:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
+var prefix = ("+")
 
 bot.on('ready', function () {
+  bot.user.SetActivity("+help")
   console.log("Je suis connecté !")
 })
 
-bot.login("process.env.TOKEN")
-
-bot.on('message', message => {
-  if (message.content === '+avatar') {
-    message.reply(message.author.avatarURL);
-  }
-})
-
-bot.on("guildMemberAdd", member => {
-    member.guild.channels.find("name","accueil").send(`Bienvenue, ${member}`)
-})
-
-bot.on("guildMemberRemove", member => {
-  member.guild.channels.find("name","accueil").send(`${member}, a décidé de fuir !`)
-})
-
-bot.on('guildMemberAdd', member => {
-    var role = member.guild.roles.find('name', 'Membre')
-    member.addRole(role)
-})
-
-bot.on('guildMemberAdd', member => {
-  member.createDM().then(channel => {
-    return channel.send('Bienvenue sur mon serveur' + member.displayName)
-  }).catch(console.error)
-})
-
 bot.on('message', msg => {
-  if (msg.content === '+ping') {
-    msg.reply('Pong!');
+  if (msgmsg.content === prefix + 'ping') {
+    msg.reply('pong !')
   }
-});
+})
+bot.login('process.env.TOKEN')
